31904f9545c42022c489173ebd18f2b7aae7202f
Triet Nguyen <tnguyen@sentient-energy.com>
2017-01-23 18:50:46 -0800

    temp fix

--- stm32flash.rb
+++ stm32flash.rb
@@ -3,8 +3,8 @@ require "formula"
 class Stm32flash < Formula
   homepage "https://code.google.com/p/stm32flash/"
   version "20160211"
-  url "https://sourceforge.net/projects/stm32flash/files/stm32flash-0.5.tar.gz/download"
-  sha256 "97aa9422ef02e82f7da9039329e21a437decf972cb3919ad817f70ac9a49e306"
+  url "https://sourceforge.net/code-snapshots/git/s/st/stm32flash/code.git/stm32flash-code-8c4aa650bffaf98e96d1b6065ab6e76c43150d8a.zip"
+  #sha256 "97aa9422ef02e82f7da9039329e21a437decf972cb3919ad817f70ac9a49e306"
 
   def install
     system "make"
